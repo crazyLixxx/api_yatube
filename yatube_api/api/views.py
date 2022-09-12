1f94303d1f59f82bcfcc56497d75ef03526af3cd
Константин Николаев <crazylixx@yandex.ru>
2022-09-12 20:46:00 +0300

    added permissions check, split urls.py and used get_object_or_404 in views

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,30 +1,30 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
-from rest_framework.exceptions import PermissionDenied
 
-from posts.models import Post, Group, Comment
+from .permissions import AuthorOrReadOnly
 from .serializers import (
     PostSerializer,
     GroupSerializer,
     CommentSerializer
 )
+from posts.models import Post, Group
 
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
+    def get_permissions(self):
+        if self.action in [
+            'update',
+            'partial_update',
+            'destroy'
+        ]:
+            return (AuthorOrReadOnly(),)
+        return super().get_permissions()
+
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
-
-    def perform_update(self, serializer):
-        if serializer.instance.author != self.request.user:
-            raise PermissionDenied('Изменение чужого контента запрещено!')
-        super(PostViewSet, self).perform_update(serializer)
-
-    def perform_destroy(self, serializer):
-        if serializer.author != self.request.user:
-            raise PermissionDenied('Удаление чужого контента запрещено!')
-        super(PostViewSet, self).perform_destroy(serializer)
 
 
 class GroupViewSet(viewsets.ReadOnlyModelViewSet):
@@ -35,24 +35,24 @@ class GroupViewSet(viewsets.ReadOnlyModelViewSet):
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
 
+    def get_permissions(self):
+        if self.action in [
+            'update',
+            'partial_update',
+            'destroy'
+        ]:
+            return (AuthorOrReadOnly(),)
+        return super().get_permissions()
+
     def get_queryset(self):
         post_id = self.kwargs.get('post_id')
-        new_queryset = Comment.objects.filter(post=post_id)
+        post = get_object_or_404(Post, pk=post_id)
+        new_queryset = post.comments.all()
         return new_queryset
 
     def perform_create(self, serializer):
         post_id = self.kwargs.get('post_id')
-        post = Post.objects.get(pk=post_id)
+        post = get_object_or_404(Post, pk=post_id)
         serializer.save(
             post=post,
             author=self.request.user)
-
-    def perform_update(self, serializer):
-        if serializer.instance.author != self.request.user:
-            raise PermissionDenied('Изменение чужого контента запрещено!')
-        super(CommentViewSet, self).perform_update(serializer)
-
-    def perform_destroy(self, serializer):
-        if serializer.author != self.request.user:
-            raise PermissionDenied('Удаление чужого контента запрещено!')
-        super(CommentViewSet, self).perform_destroy(serializer)
--- yatube_api/yatube_api/urls.py
+++ yatube_api/yatube_api/urls.py
@@ -2,25 +2,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-from rest_framework.authtoken import views
-from rest_framework.routers import DefaultRouter
-
-from api.views import PostViewSet, GroupViewSet, CommentViewSet
-
-
-router = DefaultRouter()
-router.register('posts', PostViewSet)
-router.register('groups', GroupViewSet)
-router.register(
-    r'posts/(?P<post_id>\d+)/comments',
-    CommentViewSet,
-    basename='comments')
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include(router.urls)),
-    path('api/v1/api-token-auth/', views.obtain_auth_token),
+    path('api/v1/', include('api.urls', namespace='api')),
 ]
 
 
